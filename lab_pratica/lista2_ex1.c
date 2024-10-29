49c1e1c0e5b7bec89be59b77e32fa2cb61f3b2c5
LeonardoJYNoh <leonardo.njy@gmail.com>
2024-10-29 16:01:55 -0300

    Localizador no vetor

---
+++ lab_pratica/lista2_ex1.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+// @valores é um vetor contendo os n números
+// @n é a variável que diz quantos valores estão no vetor
+// @chave é um número inteiro
+// Retorno: Sua função deve retornar i se valores[i] == chave.
+// Se o valor 'chave' não está no vetor, então sua função
+// deve retornar -1
+
+
+void leitor_lista(int n, int valores[]) {
+    int i;
+    printf("Digite os valores da lista:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &valores[i]);
+    }
+}
+
+
+int busca(int valores[], int n, int chave){
+    int i;
+    for(i=0; i<n; i++) {
+        if (valores[i] == chave) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+
+
+int main(){
+
+    int n, valores[100], chave;
+    printf("digite o valor de n\n");
+    scanf("%d", &n);
+    printf("digite o valor de chave\n");
+    scanf("%d", &chave);
+    leitor_lista(n, valores);
+    printf("\no valor de i = %d\n", busca(valores, n, chave));
+    
+    return 0;
+}
+
+
+// na hora de chamar uma função que exige uma lista, so chamar a variavel sem o "[]"
+// na hora de criar uma lista é necessario colocar os "[]" sem mostrar o tamanho dela
