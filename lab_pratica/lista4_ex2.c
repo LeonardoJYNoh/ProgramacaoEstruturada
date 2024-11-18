fe2a74df4f8a4ee6ff3e011cc19129724eb0bfe4
LeonardoJYNoh <leonardo.njy@gmail.com>
2024-11-17 21:29:37 -0300

    Contador da maior sequência de '0'

---
+++ lab_pratica/lista4_ex2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#define MAX 100
+
+//Escreva um programa que devolva o tamanho da maior cadeia de 0’s de uma string
+
+int contador_sequencia_0(char str[MAX]){
+
+    int sequencia = 0;
+    int maior = 0;
+
+    for(int i = 0; str[i] != '\0' ; i++){
+        if (str[i] == '0'){
+            sequencia++;
+        } else {
+            if (sequencia > maior){
+                maior = sequencia;
+            }
+            sequencia = 0;
+        }
+
+    }
+
+    if (sequencia > maior) {
+        maior = sequencia;
+    
+    }
+
+    return maior;
+}
+
+
+int main(){
+    
+    char str[MAX];
+    fgets(str, MAX, stdin);
+    printf("%d", contador_sequencia_0(str));
+
+}
