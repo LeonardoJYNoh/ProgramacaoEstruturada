149c364afb1033bf51275e747b7da35b8469a586
LeonardoJYNoh <leonardo.njy@gmail.com>
2024-11-17 22:08:09 -0300

    Adicionar função toggleCase

--- lab_pratica/lista4_ex3.c
+++ lab_pratica/lista4_ex3.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
 #define MAX 100
 
+void inversor_CAPS(char str[MAX]){
+
+    for(int i = 0; str[i] != '\0'; i++){
+        if (str[i] >= 'A' && str[i] <= 'Z'){
+            printf("%c", str[i] + 32);
+        }
+        else if (str[i] >= 'a' && str[i] <= 'z') {
+            printf("%c", str[i] - 32);
+        }
+        else {
+            printf("%c", str[i]);
+        }
+    }
+
+}
+
+
 int main(){
-    int str[MAX];
+    char str[MAX];
+    printf("Digite uma frase, para transforma maiusculas em minusculas e vice-versa\n");
     fgets(str, MAX, stdin);
-    
+    inversor_CAPS(str);
 
 }
